ef7355ceb2968347e837483a71e02107b35fcded
Siddhi Shankar <siddhishankar16@gmail.com>
2026-06-17 22:08:33 +0530

    created footeitem file

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -8,6 +8,20 @@ const FoodDisplay = (category) => {
   return (
     <div className="food-display" id="food-display">
       <h2>Top Dishes Near You</h2>
+      <div className="food-display-list">
+        {food_list.map((item, index) => {
+          return (
+            <FoodItem
+              key={index}
+              id={item._id}
+              name={item.name}
+              description={item.description}
+              price={item.price}
+              image={item.image}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 };
